9324a681a25ea499a495b42e6db2a86723e6ddf3
Pawloland <59684145+Pawloland@users.noreply.github.com>
2025-04-05 20:40:02 +0200

    Add option to disable creation of logfiles via env var

--- src/BTS/QtApplicationEnvironment/ApplicationEnvironment.cpp
+++ src/BTS/QtApplicationEnvironment/ApplicationEnvironment.cpp
@@ -93,6 +93,24 @@ BtsId ApplicationEnvironment::generateBtsId()
 
 std::string ApplicationEnvironment::logFilename(BtsId btsId)
 {
+    // Check if the environment variable DO_NOT_LOG_TO_FILE is set to "1" and if so
+    // return a null device path to avoid automatic creation of log file by a subsequent
+    // logger initialization
+    // This is to prevent pollution of filesystem with log files
+    const char *doNotLogEnv = std::getenv("DO_NOT_LOG_TO_FILE");
+    if (doNotLogEnv && std::string(doNotLogEnv) == "1")
+    {
+        // Using std::cerr to indicate that logging to file is disabled, explicitly without
+        // relying on the logger itself
+        std::cerr << "Note: DO_NOT_LOG_TO_FILE is set to 1, no log file will be created."
+                  << std::endl;
+
+#ifdef _WIN32
+        return "NUL";
+#else
+        return "/dev/null";
+#endif
+    }
     auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     auto localNow = localtime(&now);
     char timeBuff[20];
